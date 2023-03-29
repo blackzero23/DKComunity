3f934654e0d2a8174ba64daddbb59bbc3e82546b
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-03-29 16:33:07 +0900

    스프링 연습 프로젝트 생성

---
+++ src/main/java/com/dokyun/DKComunity/DkComunityApplication.java
@@ -0,0 +1,13 @@
+package com.dokyun.DKComunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DkComunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DkComunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dokyun/DKComunity/DkComunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dokyun.DKComunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DkComunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
