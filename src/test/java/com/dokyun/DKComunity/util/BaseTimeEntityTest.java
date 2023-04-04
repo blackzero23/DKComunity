a06a80c7fe4ded44b6203092b4c13e075a9c58b0
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-04-04 21:17:26 +0900

    생성일시, 생성자, 수정일시, 수정자 테스트 완료

--- src/test/java/com/dokyun/DKComunity/util/BaseTimeEntityTest.java
+++ src/test/java/com/dokyun/DKComunity/util/BaseTimeEntityTest.java
@@ -1,13 +1,11 @@
 package com.dokyun.DKComunity.util;
 
-import com.dokyun.DKComunity.domain.Members;
+import com.dokyun.DKComunity.domain.Member;
 import com.dokyun.DKComunity.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class BaseTimeEntityTest {
@@ -19,7 +17,7 @@ class BaseTimeEntityTest {
     @DisplayName("BaseTimeEntity_등록시 생성자_수정자_생성시간_수정시간_자동입력")
     void BaseTimeEntity_등록시_생성자_수정자_생성시간_수정시간_자동입력() {
         //given
-        Members member = new Members();
+        Member member = new Member();
         member.setNickName("test");
         member.setEmail("test@email");
         member.setPassword("test");
@@ -27,7 +25,7 @@ class BaseTimeEntityTest {
         //when
         memberRepository.save(member);
         //then
-        Members findMember = memberRepository.findById(member.getId()).get();
+        Member findMember = memberRepository.findById(member.getId()).get();
         System.out.println("findMember.getCreatedDate() = " + findMember.getCreateAt());
         System.out.println("findMember.getCreatedDate() = " + findMember.getCreateBy());
     }
