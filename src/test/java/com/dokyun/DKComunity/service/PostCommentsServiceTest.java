00bf27b311c3f70a50c0d58c48010bbc3ec92501
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-04-17 23:10:20 +0900

    게시물 댓글 관련 징행중이나 jpql 에러 해결중.

--- src/test/java/com/dokyun/DKComunity/service/PostCommentsServiceTest.java
+++ src/test/java/com/dokyun/DKComunity/service/PostCommentsServiceTest.java
@@ -1,40 +1,58 @@
 package com.dokyun.DKComunity.service;
 
-import com.dokyun.DKComunity.dto.post.PostCommentCreateDto;
-import com.dokyun.DKComunity.dto.post.PostCommentInfoDto;
-import com.dokyun.DKComunity.dto.post.PostCreateDto;
-import com.dokyun.DKComunity.dto.post.PostInfoDto;
+import com.dokyun.DKComunity.domain.Member;
+import com.dokyun.DKComunity.domain.PostComments;
+import com.dokyun.DKComunity.domain.Posts;
+import com.dokyun.DKComunity.domain.PostsCategory;
+import com.dokyun.DKComunity.repository.MemberRepository;
+import com.dokyun.DKComunity.repository.PostCategoryRepository;
+import com.dokyun.DKComunity.repository.PostCommentsRepository;
+import com.dokyun.DKComunity.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @SpringBootTest
 class PostCommentsServiceTest {
     @Autowired
+    PostCommentsRepository postCommentsRepository;
+    @Autowired
     PostCommentsService postCommentsService;
-
+    @Autowired
+    PostRepository postRepository;
     @Autowired
     PostService postService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    PostCategoryRepository postCategoryRepository;
     @Test
     @DisplayName("댓글 추가")
     void addPostComment() {
-        //given
-        PostInfoDto post = postService.createPost(new PostCreateDto("test", "test", 1L));
+//        //given
+//        PostInfoDto post = postService.createPost(new PostCreateDto("test", "test", 1L));
+//
+//        PostCommentCreateDto postCommentCreateDto = new PostCommentCreateDto();
+//        postCommentCreateDto.setPostId(1L);
+//        postCommentCreateDto.setMemberId(1L);
+//        postCommentCreateDto.setContent("댓글 내용");
+//        //when
+//        PostCommentInfoDto postCommentInfoDto = postCommentsService.addPostComment(postCommentCreateDto);
+//        //then
+//        assertNotNull(postCommentInfoDto);
+//        assertEquals(postCommentInfoDto.getPostId(), postCommentCreateDto.getPostId());
+//        assertEquals(postCommentInfoDto.getMemberId(), postCommentCreateDto.getMemberId());
+//        assertEquals(postCommentInfoDto.getContent(), postCommentCreateDto.getContent());
+        Member member = Member.createMember("test", "test", "test");
+        Member savedMember = memberRepository.save(member);
+        PostsCategory category = PostsCategory.createCategory("test");
+        PostsCategory savedCategory = postCategoryRepository.save(category);
+        Posts posts = Posts.createPosts("test", "test", savedMember, savedCategory);
+        Posts savedPosts = postRepository.save(posts);
+        PostComments test = PostComments.createPostComments("test", savedMember, savedPosts);
 
-        PostCommentCreateDto postCommentCreateDto = new PostCommentCreateDto();
-        postCommentCreateDto.setPostId(1L);
-        postCommentCreateDto.setMemberId(1L);
-        postCommentCreateDto.setContent("댓글 내용");
-        //when
-        PostCommentInfoDto postCommentInfoDto = postCommentsService.addPostComment(postCommentCreateDto);
-        //then
-        assertNotNull(postCommentInfoDto);
-        assertEquals(postCommentInfoDto.getPostId(), postCommentCreateDto.getPostId());
-        assertEquals(postCommentInfoDto.getMemberId(), postCommentCreateDto.getMemberId());
-        assertEquals(postCommentInfoDto.getContent(), postCommentCreateDto.getContent());
+
     }
 
 
