8e7c9aa31329c4aa62380ab25de30dbbd5c03467
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-04-20 23:53:00 +0900

    게시물 댓글 관련 징행중이나 JPQL 잠시 주석 처리

--- src/main/java/com/dokyun/DKComunity/repository/PostCommentsBadRepository.java
+++ src/main/java/com/dokyun/DKComunity/repository/PostCommentsBadRepository.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface PostCommentsBadRepository extends JpaRepository<PostCommentsBad,Long> {
-    @Query("select p from PostCommentsBad p join fetch p.member join fetch  p.postComments where p.member.id = :memberId")
+//    @Query("select p from PostCommentsBad p join fetch p.member join fetch  p.postComments where p.member.id = :memberId")
     Page<PostCommentsBad> findByMemberId(@Param("memberId") Long memberId, Pageable pageable);
 }
