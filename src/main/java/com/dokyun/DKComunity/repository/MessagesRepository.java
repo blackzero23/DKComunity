7a793878750b8181a234e66d3904a52340cdec8e
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-04-25 21:12:20 +0900

    fetch join 에러 해결완료했으나
    다른 비슷한 기능은 querydsl로 시도해보는것도 좋을듯 하다.

--- src/main/java/com/dokyun/DKComunity/repository/MessagesRepository.java
+++ src/main/java/com/dokyun/DKComunity/repository/MessagesRepository.java
@@ -4,8 +4,11 @@ import com.dokyun.DKComunity.domain.Messages;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MessagesRepository extends JpaRepository<Messages, Long> {
     //받은사람 메세지 리스트
+    @Query(value = "select m from Messages m join fetch m.receiverId where m.receiverId = :receiverId",
+            countQuery = "select count(m) from Messages m join m.receiverId where m.receiverId = :receiverId")
     Page<Messages> findByReceiverId(Long receiverId, Pageable pageable);
 }
