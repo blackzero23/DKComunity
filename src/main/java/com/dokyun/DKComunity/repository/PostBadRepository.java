25b0d763936f43c07fe6025416a248cdf8639d9a
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-04-27 22:20:43 +0900

    method 이름 변경 + jpql 추가

--- src/main/java/com/dokyun/DKComunity/repository/PostBadRepository.java
+++ src/main/java/com/dokyun/DKComunity/repository/PostBadRepository.java
@@ -4,6 +4,7 @@ import com.dokyun.DKComunity.domain.PostsBad;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
@@ -12,5 +13,7 @@ public interface PostBadRepository extends JpaRepository<PostsBad, Long> {
 
     Optional<PostsBad> findByPostsId(Long postsId);
 
-    Page<PostsBad> findByMember(Long memberId, Pageable pageable);
+    @Query(value = "select pb from PostsBad pb join fetch pb.member join fetch pb.posts where pb.member.id = :memberId",
+            countQuery = "select count(pb) from PostsBad pb join pb.member join pb.posts where pb.member.id = :memberId")
+    Page<PostsBad> findByMemberId(Long memberId, Pageable pageable);
 }
