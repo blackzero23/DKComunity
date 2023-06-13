683d9a0831e02081740b99d15c76858019a2cbef
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-06-13 18:54:50 +0900

    Member 삭제 테스트 완료. Member 삭제 테스트는 가능하나 앞으로의 정책을 다시 생각해볼필요는 있음 예) 블라인드 처리, 1년후 삭제등.

--- src/test/java/com/dokyun/DKComunity/service/MemberServiceImplTest.java
+++ src/test/java/com/dokyun/DKComunity/service/MemberServiceImplTest.java
@@ -8,6 +8,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
 class MemberServiceImplTest {
 
@@ -37,5 +39,20 @@ class MemberServiceImplTest {
         memberRepository.save(savedMember);
         //then
         Assertions.assertThat(savedMember.getNickName()).isEqualTo("test2");
+    }
+
+    @Test
+    @DisplayName("회원 삭제 테스트")
+    void givenMember_whenDelete_thenSuccess() {
+        //given
+        Member member = Member.createMember("test", "test", "test@gmail.com");
+        Member savedMember = memberRepository.save(member);
+
+        //when
+        memberRepository.delete(savedMember);
+        //then
+        List<Member> memberList = memberRepository.findAll();
+        Assertions.assertThat(memberList.size()).isEqualTo(0);
+
     }
 }
