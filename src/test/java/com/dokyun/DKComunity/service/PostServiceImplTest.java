c896e5e73c81605152a30eebc1ef8746c51447e6
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-06-18 23:09:25 +0900

    post Create Test Service 부분 헷갈렸는데 내가 잘못 본거였음. 잘되는데 뭔가 많은부분이 추가되어서 추후에 수정할 필요를 느낌.

--- src/test/java/com/dokyun/DKComunity/service/PostServiceImplTest.java
+++ src/test/java/com/dokyun/DKComunity/service/PostServiceImplTest.java
@@ -3,33 +3,57 @@ package com.dokyun.DKComunity.service;
 import com.dokyun.DKComunity.domain.Member;
 import com.dokyun.DKComunity.domain.Posts;
 import com.dokyun.DKComunity.domain.PostsCategory;
+import com.dokyun.DKComunity.dto.member.MemberSignupDto;
+import com.dokyun.DKComunity.dto.post.PostCateCreateDto;
+import com.dokyun.DKComunity.dto.post.PostCreateDto;
+import com.dokyun.DKComunity.dto.post.PostInfoDto;
 import com.dokyun.DKComunity.repository.MemberRepository;
 import com.dokyun.DKComunity.repository.PostCategoryRepository;
 import com.dokyun.DKComunity.repository.PostRepository;
+import com.dokyun.DKComunity.service.serviceImpl.MemberServiceImpl;
+import com.dokyun.DKComunity.service.serviceImpl.PostCategoryServiceImpl;
+import com.dokyun.DKComunity.service.serviceImpl.PostServiceImpl;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import java.util.Optional;
 
 @SpringBootTest
 class PostServiceImplTest {
-
     @Autowired
     PostService postService;
     @Autowired
     MemberService memberService;
     @Autowired
     PostCategoryService postCategoryService;
-
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    PostCategoryRepository postCategoryRepository;
+    @Autowired
+    PostRepository postRepository;
 
     @DisplayName("게시판 생성")
+    @Test
     void postCreate(){
-//        memberService.
+        //given
+        MemberSignupDto memberSignupDto = MemberSignupDto.builder().nickName("test").email("test@gmail.com").password("test").build();
+        memberService.signup(memberSignupDto);
+        postCategoryService.createPostCategory(PostCateCreateDto.builder().title("quant").build());
+        Optional<PostsCategory> postsCategory = postCategoryRepository.findByTitle("quant");
+        PostInfoDto post = postService.createPost(PostCreateDto.builder().categoryId(1L).memberId(1L).content("내용 없음").title("테스트한다!").build());
+        //when
+        Optional<Posts> posts = postRepository.findById(1L);
+        Posts post1 = null;
+        if(posts.isPresent()){
+            post1 = posts.get();
+            Assertions.assertThat("테스트한다!").isEqualTo(post1.getTitle());
+        }
+        //then
+
 
 
     }
