3d5ef54ea1f307417186a5fc3a45ad1ef62c999c
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-06-20 23:29:54 +0900

    현재 포스트 에대한 좋아요 개수 추가.

--- src/main/java/com/dokyun/DKComunity/repository/PostsGoodRepository.java
+++ src/main/java/com/dokyun/DKComunity/repository/PostsGoodRepository.java
@@ -8,9 +8,10 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
-
-public interface PostGoodRepository extends JpaRepository<PostsGood, Long> {
+public interface PostsGoodRepository extends JpaRepository<PostsGood, Long> {
     @Query("select pg from PostsGood pg join  pg.member join pg.posts where pg.member.id = :memberId")
     Page<PostsGood> findByMember(Member member, Pageable pageable);
+
+    //현재 게시판에 대한 좋아요 개수.
+    long countByPosts(Posts posts);
 }
