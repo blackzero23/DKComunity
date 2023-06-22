4788f59d59461ba5c56bf70d64dea82daeeb0e56
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-06-22 23:06:04 +0900

    포스트 좋아요 기본 기능 테스트 완료. 생성, 삭제, 좋아요 개수.

--- src/test/java/com/dokyun/DKComunity/repository/PostGoodRepositoryTest.java
+++ src/test/java/com/dokyun/DKComunity/repository/PostGoodRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -23,6 +24,48 @@ class PostGoodRepositoryTest {
     PostRepository postRepository;
     @Autowired
     PostsGoodRepository postsGoodRepository;
+
+    //생성
+    @Test
+    @DisplayName("포스트 좋아요 생성")
+    void createPostsGood(){
+        //given
+        Member member = Member.builder().nickName("test").email("test@gmail.com").password("test").build();
+        PostsCategory postsCategory = PostsCategory.builder().title("quant").build();
+        Member savedMember = memberRepository.save(member);
+        PostsCategory savedPostsCategory = postCategoryRepository.save(postsCategory);
+        Posts posts = Posts.builder().member(savedMember).postsCategory(savedPostsCategory).content("postsCotent").title("title!!!").build();
+        Posts savedPosts = postRepository.save(posts);
+        PostsGood postsGood = PostsGood.builder().member(savedMember).posts(savedPosts).build();
+        //when
+        PostsGood savedPostsGood = postsGoodRepository.save(postsGood);
+
+        //then
+        Assertions.assertThat(savedPostsGood.getId()).isEqualTo(1L);
+
+    }
+
+
+    //삭제
+    @Test
+    @Transactional
+    @DisplayName("포스트 좋아요 삭제")
+    void deletePostsGood(){
+        //given
+        Member member = Member.builder().nickName("test").email("test@gmail.com").password("test").build();
+        PostsCategory postsCategory = PostsCategory.builder().title("quant").build();
+        Member savedMember = memberRepository.save(member);
+        PostsCategory savedPostsCategory = postCategoryRepository.save(postsCategory);
+        Posts posts = Posts.builder().member(savedMember).postsCategory(savedPostsCategory).content("postsCotent").title("title!!!").build();
+        Posts savedPosts = postRepository.save(posts);
+        PostsGood postsGood = PostsGood.builder().member(savedMember).posts(savedPosts).build();
+        //when
+        PostsGood savedPostsGood = postsGoodRepository.save(postsGood);
+        postsGoodRepository.delete(savedPostsGood);
+
+        //then
+        Assertions.assertThat(0L).isEqualTo(postsGoodRepository.findAll().size());
+    }
 
     @Test
     @DisplayName("포스트에 대한 좋아요 개수")
