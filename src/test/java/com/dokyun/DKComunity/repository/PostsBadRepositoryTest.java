ec7f1513f99ed12815bd33cbf784695fd42585c7
blackzero23 <49605999+blackzero23@users.noreply.github.com>
2023-06-22 23:06:14 +0900

    포스트 싫어요 기본 기능 테스트 완료. 생성, 삭제, 싫어요 개수.

--- src/test/java/com/dokyun/DKComunity/repository/PostsBadRepositoryTest.java
+++ src/test/java/com/dokyun/DKComunity/repository/PostsBadRepositoryTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,9 +23,49 @@ class PostsBadRepositoryTest {
     PostRepository postRepository;
     @Autowired
     PostsBadRepository postsBadRepository;
+
+    //생성
+    @DisplayName("포스트 생성")
+    @Test
+    void createPostsBad() {
+        //given
+        Member member = Member.builder().nickName("test").email("test@gmail.com").password("test").build();
+        PostsCategory postsCategory = PostsCategory.builder().title("quant").build();
+        Member savedMember = memberRepository.save(member);
+        PostsCategory savedPostsCategory = postCategoryRepository.save(postsCategory);
+        Posts posts = Posts.builder().member(savedMember).postsCategory(savedPostsCategory).content("postsCotent").title("title!!!").build();
+        Posts savedPosts = postRepository.save(posts);
+        PostsBad postsBad = PostsBad.builder().member(savedMember).posts(posts).build();
+        //when
+        PostsBad savedPostsBad = postsBadRepository.save(postsBad);
+        //then
+        Assertions.assertThat(savedPostsBad.getId()).isEqualTo(1L);
+    }
+
+    //삭제
+    @Test
+    @DisplayName("포스트 싫어요 삭제")
+    @Transactional
+    void deletePostsBad() {
+        //given
+        Member member = Member.builder().nickName("test").email("test@gmail.com").password("test").build();
+        PostsCategory postsCategory = PostsCategory.builder().title("quant").build();
+        Member savedMember = memberRepository.save(member);
+        PostsCategory savedPostsCategory = postCategoryRepository.save(postsCategory);
+        Posts posts = Posts.builder().member(savedMember).postsCategory(savedPostsCategory).content("postsCotent").title("title!!!").build();
+        Posts savedPosts = postRepository.save(posts);
+        PostsBad postsBad = PostsBad.builder().member(savedMember).posts(posts).build();
+        //when
+        PostsBad savedPostsBad = postsBadRepository.save(postsBad);
+        postsBadRepository.delete(savedPostsBad);
+
+        //then
+        Assertions.assertThat(0L).isEqualTo(postsBadRepository.findAll().size());
+    }
+
     @DisplayName("포스트에 대한 싫어요 개수")
     @Test
-    void countPostsBad(){
+    void countPostsBad() {
         //맴버 생성, 카테고리생성, 포스트 생성, 좋아요.
         ///given
         Member member = Member.builder().nickName("test").email("test@gmail.com").password("test").build();
